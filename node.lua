48b5405c5019a34e971dad9d19bb76ea63216d7f
Samuel Linde <samuel@dontblink.se>
2018-03-16 10:28:21 +0100

    More timer tests

--- node.lua
+++ node.lua
@@ -2,17 +2,16 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 local font = resource.load_font("silkscreen.ttf")
 
+local time
 util.json_watch("config.json", function(config)
     time = config.time
 end)
 
 util.set_interval(1, function()
-
+    time = time - 1
+end)
 
 node.render()
-    util.set_interval(1, function()
-        time = time - 1
-        gl.clear(0, 0, 0, 1)
-        font:write(120, 320, time, 100, 1, 1, 1, 1)
-    end)
+    gl.clear(0, 0, 0, 1)
+    font:write(120, 320, time, 100, 1, 1, 1, 1)
 end)
