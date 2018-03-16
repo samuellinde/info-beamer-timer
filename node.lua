fe149fbb03ef82342a00c6d1c743802ae66faff5
Samuel Linde <samuel@dontblink.se>
2018-03-16 10:24:17 +0100

    More timer tests

--- node.lua
+++ node.lua
@@ -6,8 +6,13 @@ util.json_watch("config.json", function(config)
     time = config.time
 end)
 
-util.set_interval(1, function node.render()
-    gl.clear(0, 0, 0, 1)
-    time = time - 1
-    font:write(120, 320, time, 100, 1, 1, 1, 1)
+util.set_interval(1, function()
+
+
+node.render()
+    util.set_interval(1, function()
+        time = time - 1
+        gl.clear(0, 0, 0, 1)
+        font:write(120, 320, time, 100, 1, 1, 1, 1)
+    end)
 end)
