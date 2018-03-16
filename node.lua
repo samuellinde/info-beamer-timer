44f45105c72400c8d2ab5947b34f2cda19bcc3e0
Samuel Linde <samuel@dontblink.se>
2018-03-16 10:56:37 +0100

    Debugging

--- node.lua
+++ node.lua
@@ -21,10 +21,10 @@ end)
 
 function node.render()
     gl.clear(0, 0, 0, 1)
-    title_width = font:write(title, title_size)
-    title_x = NATIVE_WIDTH / 2 - title_width / 2
-    time_width = font:write(time, timer_size)
-    time_x = NATIVE_WIDTH / 2 - time_width / 2
+    -- title_width = font:write(title, title_size)
+    -- title_x = WIDTH / 2 - title_width / 2
+    -- time_width = font:write(time, timer_size)
+    -- time_x = WIDTH / 2 - time_width / 2
     font:write(400, 250, title, title_size, 1, 1, 1, 1)
     font:write(400, 450, time, timer_size, 1, 1, 1, 1)
 end
