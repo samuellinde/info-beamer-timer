42470dafe862f8a4320351f0c4d93584993e2fee
Samuel Linde <samuel@dontblink.se>
2018-03-16 10:03:51 +0100

    Testing config loading

--- node.lua
+++ node.lua
@@ -1,7 +1,12 @@
-gl.setup(1920, 1080)
+gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 local font = resource.load_font("silkscreen.ttf")
 
+util.json_watch("config.json", function(config)
+    time = config.time
+end)
+
 function node.render()
-    font:write(120, 320, "Hello World", 100, 1, 1, 1, 1)
+    gl.clear(0, 0, 0, 1)
+    font:write(120, 320, time, 100, 1, 1, 1, 1)
 end
