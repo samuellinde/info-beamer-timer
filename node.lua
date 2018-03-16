f1fc7cd672bfb8bfec9336b79fddba2e5684f7ce
Samuel Linde <samuel@dontblink.se>
2018-03-16 11:28:16 +0100

    Debug

--- node.lua
+++ node.lua
@@ -14,13 +14,13 @@ local titleSize = 120
 util.json_watch("config.json", function(config)
     timer = config.time * 60
     local timerWidth = font:write(tostring(time), timerSize)
-    timerX = (NATIVE_WIDTH / 2) - (tointeger(timerWidth) / 2)
+    -- timerX = (NATIVE_WIDTH / 2) - (tointeger(timerWidth) / 2)
     -- timerStr = config.time .. ":00"
 
     -- Setup title
     title = config.title
     local titleWidth = font:write(title, titleSize)
-    titleX = (NATIVE_WIDTH / 2) - (tointeger(titleWidth) / 2)
+    -- titleX = (NATIVE_WIDTH / 2) - (tointeger(titleWidth) / 2)
 end)
 
 util.set_interval(1, function()
@@ -29,6 +29,6 @@ end)
 
 function node.render()
     gl.clear(0, 0, 0, 1)
-    font:write(titleX, 250, title, titleSize, 1, 1, 1, 1)
-    font:write(timerX, 450, tostring(time), timerSize, 1, 1, 1, 1)
+    font:write(400, 250, title, titleSize, 1, 1, 1, 1)
+    font:write(400, 450, tostring(time), timerSize, 1, 1, 1, 1)
 end
