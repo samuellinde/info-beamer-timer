783bbaea9eba6b9f4807aa1cd9394211140e01e0
Samuel Linde <samuel@dontblink.se>
2018-03-16 12:12:54 +0100

    Debug

--- node.lua
+++ node.lua
@@ -25,7 +25,7 @@ end)
 
 util.set_interval(1, function()
     timer = timer - 1
-    local minutes = string.format("%c", timer / 60)
+    local minutes = tostring(timer / 60)
     -- local seconds = timer - (minutes * 60)
     timer = minutes
     -- timerStr = tostring(minutes) .. ':' .. tostring(seconds)
