8aa587d20b40095a5dacbea0c6b65030aadcd53d
Samuel Linde <samuel@dontblink.se>
2018-03-16 10:10:03 +0100

    Testing rudimentary timer

--- node.lua
+++ node.lua
@@ -6,7 +6,8 @@ util.json_watch("config.json", function(config)
     time = config.time
 end)
 
-function node.render()
+util.set_interval(1, function node.render()
     gl.clear(0, 0, 0, 1)
+    time = time - 1
     font:write(120, 320, time, 100, 1, 1, 1, 1)
-end
+end)
