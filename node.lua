e1467a7fd133dc2efcb017501ab6f4b14c620e7c
Samuel Linde <samuel@dontblink.se>
2018-03-16 10:49:08 +0100

    More timer tweaks

--- node.lua
+++ node.lua
@@ -4,16 +4,27 @@ local font = resource.load_font("silkscreen.ttf")
 
 local time
 local time_str
+local title
+local title_size = 120
+local time_size = 200
+
 util.json_watch("config.json", function(config)
     time = config.time * 60
-    time_str = config.time .. ":00"
+    title = config.title
+    -- time_str = config.time .. ":00"
 end)
 
 util.set_interval(1, function()
     time = time - 1
+    -- time_str = 
 end)
 
 function node.render()
     gl.clear(0, 0, 0, 1)
-    font:write(120, 320, time, 100, 1, 1, 1, 1)
+    title_width = font:write(title, 120)
+    title_x = NATIVE_WIDTH / 2 - title_width / 2
+    time_width = font:write(time, 200)
+    time_x = NATIVE_WIDTH / 2 - time_width / 2
+    font:write(title_x, 250, title, 120, 1, 1, 1, 1)
+    font:write(time_x, 450, time, 200, 1, 1, 1, 1)
 end
