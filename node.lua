0bcef4b0f1ed35bcaee87ab9900173d4f45968cb
Samuel Linde <samuel@dontblink.se>
2018-03-20 13:44:46 +0100

    Another test

--- node.lua
+++ node.lua
@@ -18,7 +18,7 @@ local font = resource.load_font("silkscreen.ttf")
 local bouchblkb = resource.load_font("bouchblkb.ttf")
 
 -- Images
-local bgImage -- = resource.load_image("redwedding.jpg")
+local bgImage = resource.load_image("redwedding.jpg")
 local endImage = resource.load_image("endofround.png")
 
 -- Textures/overlays
@@ -29,7 +29,7 @@ local yTexture = resource.create_colored_texture(1, 1, 0, 0.6)
 util.file_watch("config.json", function(content)
     config = json.decode(content)
 
-    bgImage = resource.load_image(config.bgimage.asset_image)
+    -- bgImage = resource.load_image(config.bgimage)
 
     -- Set initTimer on first load
     local configTimer = config.timer * 60
