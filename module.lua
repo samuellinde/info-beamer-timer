30f7f26a198a349bdc0d2dafd5b3c24c66acd3a9
Samuel Linde <samuel@dontblink.se>
2018-03-21 14:56:08 +0100

    More tests

--- module.lua
+++ module.lua
@@ -12,7 +12,7 @@ local timerStatus = 'running'
 local timer = 3000
 local timerStr
 local timerX
-local timerSize = 40 -- 400
+local timerSize = 400 -- 400
 
 local title
 local titleX, titleSize
@@ -38,40 +38,40 @@ end
 -- Load and reload config.json
 function M.content_update(name)
     print("sub module content update", name)
-    -- if name == 'config.json' then
-    --     config = json.decode(content)
+    if name == 'config.json' then
+        config = json.decode(content)
 
-    --     -- Load background image, replace if new
-    --     if (not bgImageName) and (config.bgimage.asset_name ~= bgImageName) then
-    --         bgImageName = config.bgimage.asset_name
-    --         bgImage = resource.load_image(localized bgImageName)
-    --     end
+        -- Load background image, replace if new
+        -- if (not bgImageName) and (config.bgimage.asset_name ~= bgImageName) then
+        --     bgImageName = config.bgimage.asset_name
+        --     bgImage = resource.load_image(localized bgImageName)
+        -- end
 
-    --     -- Set initTimer on first load
-    --     local configTimer = config.timer * 60
-    --     if (not timer) or (configTimer ~= initTimer) then
-    --         initTimer = configTimer
-    --         halfTime = configTimer / 2
-    --         timer = configTimer
-    --         timerStr = string.format("%02d:00", config.timer)
-    --     end
+        -- Set initTimer on first load
+        local configTimer = config.timer * 60
+        if (not timer) or (configTimer ~= initTimer) then
+            initTimer = configTimer
+            halfTime = configTimer / 2
+            timer = configTimer
+            timerStr = string.format("%02d:00", config.timer)
+        end
 
-    --     -- Setup timer
-    --     if timer > 5999 then
-    --         timerApprox = "000:00"
-    --     else
-    --         timerApprox = "00:00"
-    --     end
-    --     timerSize = config.timersize
-    --     local timerWidth = font:width(timerApprox, timerSize)
-    --     timerX, timerY = NATIVE_WIDTH / 2 - timerWidth / 2, 400
+        -- Setup timer
+        if timer > 5999 then
+            timerApprox = "000:00"
+        else
+            timerApprox = "00:00"
+        end
+        timerSize = config.timersize
+        local timerWidth = font:width(timerApprox, timerSize)
+        timerX, timerY = NATIVE_WIDTH / 2 - timerWidth / 2, 400
 
-    --     -- Setup title
-    --     title = config.title
-    --     titleSize = config.titlesize
-    --     local titleWidth = bouchblkb:width(title, titleSize)
-    --     titleX, titleY = NATIVE_WIDTH / 2 - titleWidth / 2, timerY - titleSize - 60
-    -- end
+        -- Setup title
+        title = config.title
+        titleSize = config.titlesize
+        local titleWidth = bouchblkb:width(title, titleSize)
+        titleX, titleY = NATIVE_WIDTH / 2 - titleWidth / 2, timerY - titleSize - 60
+    end
 end
 
 function M.content_remove(name)
@@ -100,17 +100,17 @@ end)
 function M.draw()
     -- font:write(100, 100, timer, timerSize, 1,1,1,1)
     bgImage:draw(0, 0, NATIVE_WIDTH, NATIVE_HEIGHT)
-    -- if timer < 0 then
-    --     endImage:draw(0, 0, NATIVE_WIDTH, NATIVE_HEIGHT)
-    -- elseif timer < 180 then
-    --     yTexture:draw(0, 0, NATIVE_WIDTH, NATIVE_HEIGHT)
-    --     bouchblkb:write(titleX, titleY, title, titleSize, 0, 0, 0, 1)
-    --     font:write(timerX, timerY, timerStr, timerSize, 0, 0, 0, 1)
-    -- else
-    --     stdTexture:draw(0, 0, NATIVE_WIDTH, NATIVE_HEIGHT)
-    --     bouchblkb:write(titleX, titleY, title, titleSize, 1, 1, 1, 1)
-    --     font:write(timerX, timerY, timerStr, timerSize, 1, 1, 1, 1)
-    -- end
+    if timer < 0 then
+        endImage:draw(0, 0, NATIVE_WIDTH, NATIVE_HEIGHT)
+    elseif timer < 180 then
+        yTexture:draw(0, 0, NATIVE_WIDTH, NATIVE_HEIGHT)
+        bouchblkb:write(titleX, titleY, title, titleSize, 0, 0, 0, 1)
+        font:write(timerX, timerY, timerStr, timerSize, 0, 0, 0, 1)
+    else
+        stdTexture:draw(0, 0, NATIVE_WIDTH, NATIVE_HEIGHT)
+        bouchblkb:write(titleX, titleY, title, titleSize, 1, 1, 1, 1)
+        font:write(timerX, timerY, timerStr, timerSize, 1, 1, 1, 1)
+    end
 end
 
 return M
